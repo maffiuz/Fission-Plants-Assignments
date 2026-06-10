14221a48c2cc7114d988c34a78aae8ca16dd0fdb
Prync-e <maffiodor@gmail.com>
2026-06-10 14:40:08 +0200

    Bugfix

--- data/assignment_data.py
+++ data/assignment_data.py
@@ -15,7 +15,7 @@ sigma_ultimate = 413            # MPa
 
 # Fuel characteristics
 BU = 60000*1e-3                 # MWd/kg, fuel burnup factor                  
-rho_UO2 = 0.955 * 10960         # kg m-3, UO2 pellet density
+rho_UO2 = 0.955 * 10970         # kg m-3, UO2 pellet density
 e_U235 = 0.0445                 # -, UO2 max enrichment
 Rr = 0.4                        # -, fuel gas release ratio
 c_N2 = 25*1e-6                  # -, N2 impurity mass concentration in fuel
